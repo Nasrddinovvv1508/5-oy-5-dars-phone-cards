311875c879c5fcf84ddfea06e8509aa0a060a40c
Nasrddinovvv1508 <najmiddinnasriddinov5@gmail.com>
2024-05-05 11:41:49 +0500

    description done

--- static/JS/functions.js
+++ static/JS/functions.js
@@ -1,3 +1,5 @@
+import data from "./data.js";
+
 
 function createCard(product) {
     // Create phone_card
@@ -13,6 +15,9 @@ function createCard(product) {
     img.src = product.thumbnail;
     img.setAttribute('alt', 'photo');
 
+    let i = document.createElement('i');
+    i.textContent = product.id;
+
     let h1 = document.createElement('h1');
     h1.textContent = product.title;
 
@@ -21,7 +26,7 @@ function createCard(product) {
 
     let h3 = document.createElement(`h3`);
     h3.textContent = `${product.price}$`;
-    up.append(img, h1, p, h3);
+    up.append(i, img, h1, p, h3);
 
     // Create phone_card down;
     let down = document.createElement(`div`);
@@ -29,15 +34,44 @@ function createCard(product) {
     let button = document.createElement('button')
     button.textContent = `More`
     button.classList.add(`btn-more`);
-    button.setAttribute('onclick', btnListener(button));
     down.append(button);
 
     phoneCard.append(up, down);
     return phoneCard
 }
 
-function btnListener(button) {
-    return button.parentNode;
+function createDiv(product) {
+    let phoneDescription = document.querySelector('.phone_description')
+    let img = document.createElement(`img`);
+    img.src = product.thumbnail;
+    img.setAttribute(`alt`, `phone`);
+
+    let i = document.createElement(`i`);
+    i.textContent = product.id;
+
+    let h1 = document.createElement('h1');
+    h1.textContent = product.title;
+
+    let p = document.createElement(`p`);
+    p.textContent = product.description;
+
+    let h3 = document.createElement('h3');
+    h3.textContent = `${product.price}$`;
+
+    let divStar = document.createElement('div');
+    divStar.innerHTML = `
+        <span class="star" data-value="1">&#9733;</span>
+        <span class="star" data-value="2">&#9733;</span>
+        <span class="star" data-value="3">&#9733;</span>
+        <span class="star" data-value="4">&#9733;</span>
+        <span class="star" data-value="5">&#9733;</span>
+    `;
+    divStar.setAttribute('class', `star-rating`)
+
+    let a = document.createElement(`a`);
+    a.textContent = product.brand;
+
+    phoneDescription.append(img, i, h1, p, h3, divStar, a)
 }
 
-export {createCard}
+export {createCard, createDiv};
--- static/JS/index.js
+++ static/JS/index.js
@@ -1,5 +1,5 @@
 import data from "./data.js";
-import { createCard } from "./functions.js";
+import { createCard, createDiv } from "./functions.js";
 
 let phoneCards = document.querySelector('.phone_cards')
 
@@ -7,4 +7,30 @@ data.forEach((product) => {
     phoneCards.append(createCard(product));
 })
 
+let phoneDesc = document.querySelector(`.phone_description`);
+
 let button = document.querySelectorAll('.btn-more');
+
+let id;
+let phoneDescL = 0;
+button.forEach((v) => {
+    v.addEventListener('click', function () {
+        phoneDesc.style.display = `block`;
+        id = this.parentNode.parentNode.firstChild.firstChild.textContent;
+
+        data.forEach((product) => {
+            if (product.id == id) {
+                createDiv(product);
+            }
+        })
+        phoneDescL = phoneDesc.children.length;
+        console.log(phoneDescL);
+    });
+})
+console.log(phoneDescL);
+
+// if (!phoneDesc.children) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
